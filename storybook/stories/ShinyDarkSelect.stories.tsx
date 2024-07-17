51048c1bc1f7ea73766d9f8c2f233a2462618a18
luca.campli <campli.luca@gmail.com>
2024-07-17 19:42:24 +0200

    improve shiny dark select

--- storybook/stories/ShinyDarkSelect.stories.tsx
+++ storybook/stories/ShinyDarkSelect.stories.tsx
@@ -1,14 +1,14 @@
 import type { Meta } from '@storybook/react';
-import * as React from 'react';
 import Select, {
-  components as Components,
   useGetClassNames,
+  useSelectContext,
   type ControlProps,
 } from 'react-select';
 import cn from 'classnames';
 
 import { Field } from '../components';
 import { flavourOptions } from '../data';
+import './shiny-dark.css';
 
 export default {
   title: 'Select/ShinyDark',
@@ -23,14 +23,13 @@ export function ShinyDark() {
         inputId="shiny-dark-select-id"
         defaultValue={flavourOptions[2]}
         options={flavourOptions}
+        className="shiny-dark-select"
         classNames={{
           control: ({ isFocused }) =>
             cn(
               'relative bg-transparent border-none',
               !isFocused && 'linear-mask'
             ),
-          placeholder: 'text-white',
-          singleValue: 'text-white',
         }}
         components={{
           Control: CustomControl,
@@ -41,15 +40,32 @@ export function ShinyDark() {
 }
 
 function CustomControl(props: ControlProps) {
-  const shouldShine = useGetClassNames('control', props).includes(
-    'linear-mask'
-  );
-  console.log(shouldShine, props);
+  const { isDisabled, isFocused } = useSelectContext();
+  const { children, innerRef, innerProps } = props;
+  const className = useGetClassNames('control', props, innerProps?.className);
+
   return (
     <>
-      <div className={cn('rounded-md relative', true && 'radial-gradient')}>
-        <Components.Control {...props} />
-        {shouldShine && (
+      <div
+        data-is-disabled={isDisabled}
+        data-is-focused={isFocused}
+        className="radial-gradient"
+      >
+        <div
+          ref={innerRef}
+          {...innerProps}
+          aria-disabled={isDisabled || undefined}
+          data-is-disabled={isDisabled}
+          data-is-focused={isFocused}
+          className={cn(
+            'rounded-md relative',
+            true && 'radial-gradient',
+            className
+          )}
+        >
+          {children}
+        </div>
+        {!isFocused && (
           <span className="block absolute inset-0 rounded-md p-1 linear-overlay" />
         )}
       </div>
